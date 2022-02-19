e86391a05e47e465e62ebbf4fce22d08669d1b59
Juan Gonzalez <jgu1@hotmail.com>
2022-02-19 17:55:30 -0300

    Create services to be used for controller

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -11,4 +11,30 @@ export class CoffeesService {
       flavors: ['chocolate', 'vanilla'],
     },
   ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // Update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
 }
