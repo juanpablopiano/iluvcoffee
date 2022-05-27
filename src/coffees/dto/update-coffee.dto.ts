53ed6ae70ffc10c67ee5c978ecdb1e2a2f8be57f
Juan Gonzalez <jgu1@hotmail.com>
2022-05-27 13:21:19 -0400

    Enabling CLI plugin

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types'; this one doesn't work with swagger
+import { PartialType } from '@nestjs/swagger';
 import { CreateCoffeeDto } from './create-coffee.dto';
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
