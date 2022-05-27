50f64a650801166f9d0e9145342bbd081ae133d3
Juan Gonzalez <jgu1@hotmail.com>
2022-05-27 17:38:22 -0400

    Creating a mongoose model

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee, CoffeSchema } from './entities/coffee.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coffee.name,
+        schema: CoffeSchema,
+      },
+    ]),
+  ],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
