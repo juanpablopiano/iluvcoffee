2e11d5a0aa0079d5b46ca0bc2d0a9ac11e479492
Juan Gonzalez <user_option17@skyairline.com>
2022-05-26 20:44:47 -0400

    Finish chapter 5

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
+// import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      // validationSchema: Joi.object({
+      //   DATABASE_HOST: Joi.required(),
+      //   DATABASE_PORT: Joi.number().default(5432),
+      // }),
+      load: [appConfig],
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
